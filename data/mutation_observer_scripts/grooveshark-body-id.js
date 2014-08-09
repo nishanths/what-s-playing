d949c3a249bd003e0bf217f6a6abfbf6bc6307ef
nishanths <nishanth.gerrard@gmail.com>
2014-08-08 18:40:04 -0700

    Adds notification when restarting song on Grooveshark
    making it consistent with spottily, songza behaviors

--- data/mutation_observer_scripts/grooveshark-body-id.js
+++ data/mutation_observer_scripts/grooveshark-body-id.js
@@ -1,17 +1,30 @@
-var target = document.getElementById('now-playing-metadata');
-var options = { attributes: true, childList: true, characterData: true };
+var target_metadata = document.getElementById('now-playing-metadata'),
+    target_play_pause = document.getElementById('play-pause'),
+    options = { attributes: true, childList: true, characterData: true };
 
-var observer = new MutationObserver(function(mutations) {
+var observer_metadata = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) { 
     if (addon_exists){ 
-      self.postMessage(mutation.target.textContent);
+      self.postMessage(mutation.target_metadata.textContent);
     }
   });
-}); 
+});
+
+var observer_play_pause = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) { 
+    if (addon_exists){
+      var started_playing = document.getElementById('play-pause').outerHTML;
+      if (/class="\S* \S* playing/.test(started_playing)){
+        self.postMessage(target_metadata.textContent);
+      }
+    }
+  });
+});
 
 var addon_exists = true;
 
-observer.observe(target, options); // set up
+observer_metadata.observe(target_metadata, options); // set up
+observer_play_pause.observe(target_play_pause, options);
 
 self.port.on("detach", function() { // to disable posting messages after add-on removal
   addon_exists = false;
